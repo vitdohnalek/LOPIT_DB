6aafe7011e80fd95d621ad9cf3183ac378a117e0
Vít Dohnálek <vitdohnalek@gmail.com>
2025-11-23 16:05:24 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -6,7 +6,8 @@ USE lopit_db;
 CREATE TABLE species (
 	species_id INT AUTO_INCREMENT PRIMARY KEY,
 	dataset_id VARCHAR(255) NOT NULL,
-	species_name VARCHAR(255) NOT NULL	
+	species_name VARCHAR(255) NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- 3. Create table of proteins
@@ -22,14 +23,27 @@ CREATE TABLE proteins (
 	sequence_length INT NOT NULL,
 	description TEXT,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
 	-- Uniqueness constraints
 	UNIQUE (uniprot_id),
 	UNIQUE (eukprot_id),
 	UNIQUE (species_id, gene_id),
 
-	FOREIGN KEY (species_id) REFERENCES species(species_id)
+	FOREIGN KEY (species_id) REFERENCES species(species_id) ON DELETE CASCADE
 );
 
+-- 4. Create table of foldseek annotations
+-- Contains top hits against three databases and the most frequent hit in the results
+CREATE TABLE foldseek (
+	protein_id INT PRIMARY KEY,
+	structure_predictor VARCHAR(255),
+	plddt FLOAT,
+	db_swiss VARCHAR(255),
+	db_uniprot VARCHAR(255),
+	db_proteomes VARCHAR(255),
+	most_frequent_hit VARCHAR(255),
+	most_frequent_hit_n INT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
+	FOREIGN KEY (protein_id) REFERENCES proteins(protein_id) ON DELETE CASCADE
+);
